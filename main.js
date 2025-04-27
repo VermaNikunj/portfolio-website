439c2275e1abd7835783b93349a23a44c36f445f
Nikunj Verma <43748023+VermaNikunj@users.noreply.github.com>
2025-04-27 18:42:58 +0530

    Initial commit
    Initial commit and 1st stable code

---
+++ main.js
@@ -0,0 +1,34 @@
+{/* <div class="timeline-item">
+    <!-- <div class="timeline-dot"></div> -->
+    <div class="timeline-left">
+        <div class="timeline-date">2021-present</div>
+    <div class="timeline-title">Frontend - Capg</div>
+    </div>
+    <div class="timeline-content">
+        <!-- <h4>2021</h4>
+        <h3>Frontend developer</h3> -->
+        frontend developer at Capgemini. started my carrer at Capgemini.started my carrer at Capgemini.started my carrer at Capgemini.
+        <ul>
+            <li>Detail 1 list item</li>
+            <li>Detail 2 list item</li>
+            <li>Detail 3 list item</li>
+        </ul>
+    </div>
+</div>
+<div class="timeline-item">
+    <!-- <div class="timeline-dot"></div> -->
+    <div class="timeline-left">
+        <div class="timeline-date">2024-present</div>
+    <div class="timeline-title">Senior - Capg</div>
+    </div>
+    <div class="timeline-content">
+        <!-- <h4>2024</h4>
+        <h3>Senior frontend developer</h3> -->
+        senior frontend developer at Capgemini. started my carrer at Capgemini.started my carrer at Capgemini.started my carrer at Capgemini.
+        <ul>
+            <li>Detail 1 list item</li>
+            <li>Detail 2 list item</li>
+            <li>Detail 3 list item</li>
+        </ul>
+    </div>
+</div> */}

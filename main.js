db591a3be8b4e8552160eb30978174561adcdeb1
Verma <nikunj.verma@capgemini.com>
2025-05-02 00:10:06 +0530

    Clean-up code. Improve css. Add GoToTop button.

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-{/* <div class="timeline-item">
-    <!-- <div class="timeline-dot"></div> -->
-    <div class="timeline-left">
-        <div class="timeline-date">2021-present</div>
-    <div class="timeline-title">Frontend - Capg</div>
-    </div>
-    <div class="timeline-content">
-        <!-- <h4>2021</h4>
-        <h3>Frontend developer</h3> -->
-        frontend developer at Capgemini. started my carrer at Capgemini.started my carrer at Capgemini.started my carrer at Capgemini.
-        <ul>
-            <li>Detail 1 list item</li>
-            <li>Detail 2 list item</li>
-            <li>Detail 3 list item</li>
-        </ul>
-    </div>
-</div>
-<div class="timeline-item">
-    <!-- <div class="timeline-dot"></div> -->
-    <div class="timeline-left">
-        <div class="timeline-date">2024-present</div>
-    <div class="timeline-title">Senior - Capg</div>
-    </div>
-    <div class="timeline-content">
-        <!-- <h4>2024</h4>
-        <h3>Senior frontend developer</h3> -->
-        senior frontend developer at Capgemini. started my carrer at Capgemini.started my carrer at Capgemini.started my carrer at Capgemini.
-        <ul>
-            <li>Detail 1 list item</li>
-            <li>Detail 2 list item</li>
-            <li>Detail 3 list item</li>
-        </ul>
-    </div>
-</div> */}
+window.onscroll = function () { 
+    scrollFunction() 
+};
+
+function getProfileElements(ev) {
+    const getParentElement = ev.target?.parentElement?.parentElement
+    const getProfileElement = getParentElement?.getElementsByClassName('timeline-profile-detail')
+    const getDownArrowElement = getParentElement?.getElementsByClassName('timeline-down')
+    const getUpArrowElement = getParentElement?.getElementsByClassName('timeline-up')
+    return { getProfileElement, getDownArrowElement, getUpArrowElement }
+}
+
+function showProfileDetail(event) {
+    const { getProfileElement, getDownArrowElement, getUpArrowElement } = getProfileElements(event)
+    getProfileElement[0].classList.add('showProfile')
+    getDownArrowElement[0].classList.remove('showArrow')
+    getUpArrowElement[0].classList.add('showArrow')
+}
+function hideProfileDetail(event) {
+    const { getProfileElement, getDownArrowElement, getUpArrowElement } = getProfileElements(event)
+    getProfileElement[0].classList.remove('showProfile')
+    getDownArrowElement[0].classList.add('showArrow')
+    getUpArrowElement[0].classList.remove('showArrow')
+}
+
+function showCertificatePreview(path, event) {
+    const certificateColClass = event.target?.parentElement?.parentElement
+    if (![...certificateColClass.classList].includes('active-preview')) {
+        const fullPath = `/assets/certificate/${path}`
+        const getCertiPreviewId = document.getElementById('certificate-preview')
+        getCertiPreviewId.children[0].classList.add('active')
+        getCertiPreviewId.children[1].setAttribute('data', fullPath)
+        const certificateRowClass = certificateColClass?.parentElement?.children
+        for (const colClass of certificateRowClass) {
+            colClass.classList.remove('active-preview')
+        }
+        certificateColClass.classList.add('active-preview')
+        setTimeout(() => getCertiPreviewId.children[0].classList.remove('active'), 200)
+    }
+}
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+function scrollFunction() {
+    const mybutton = document.getElementById("goToTopBtn")
+    mybutton.style.display = window.scrollY > 500 ? "block" : "none"
+}
+

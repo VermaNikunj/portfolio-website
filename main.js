12e827608f7497eb326b06db44a5381a06215559
Nikunj Verma <nikunjverma37@gmail.com>
2025-09-22 23:58:08 +0530

    Improvement, addition and update code structure

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", initialFunction)
+
 window.onscroll = function () { 
     scrollFunction() 
 };
@@ -48,10 +50,109 @@ function scrollFunction() {
     mybutton.style.display = window.scrollY > 500 ? "block" : "none"
 }
 
+function getResumePath () {
+    return '/assets/resume/Nikunj_Verma_Resume.pdf'
+}
+
 function downloadCVEvent () {
     const link = document.createElement('a')
-    link.href= 'https://drive.google.com/uc?export=download&id=19goSwo0eSap-_SIQWMgE1i2weAquMMzw'
-    link.target='_blank'
+    link.href = getResumePath()
+    link.download = 'Nikunj Verma Resume Software Developer'
     link.click()
 }
 
+function themeToggleEvent() {
+    const lightThemeElements = document.getElementsByClassName('light-theme-btn')
+    const darkThemeElements = document.getElementsByClassName('dark-theme-btn')
+    lightThemeElements[0].classList.toggle('active')
+    darkThemeElements[0].classList.toggle('active')
+    changeTheme()
+}
+
+function initialFunction() {
+    getSavedSettings()
+    openSettingMenu(true)
+    loadContentComponents()
+}
+
+function getSavedSettings() {
+    if (localStorage.getItem('theme') === 'light') {
+        document.body.classList.add('light-theme')
+    } else {
+        document.body.classList.remove('light-theme')
+    }
+    if(localStorage.getItem('language')) {
+        const selectedLanguage = localStorage.getItem('language')
+        loadLanguage(selectedLanguage)
+    } else {
+        localStorage.setItem('language', 'en')
+        loadLanguage('en')
+    }
+}
+
+function changeTheme() {
+    let selectedTheme = 'light'
+    if (document.body.classList.contains('light-theme')) {
+        selectedTheme = 'dark'
+    }
+    localStorage.setItem('theme', selectedTheme)
+    document.body.classList.toggle('light-theme')
+}
+
+function openSettingMenu(inStart = false) {
+    const settingMenu = document.getElementById('setting-menu')
+    if(inStart) {
+        if(settingMenu) settingMenu.style.display = 'none'
+    } else {
+        if(settingMenu) settingMenu.style.display = (settingMenu.style.display === 'flex') ? 'none' : 'flex'
+        const languageSelect = document.getElementById('language')
+        if(languageSelect) languageSelect.value = localStorage.getItem('language') || 'en'
+    }
+}
+
+function languageChangeEvent() {
+    const languageSelect = document.getElementById('language')
+    const selectedLanguage = languageSelect.value
+    localStorage.setItem('language', selectedLanguage)
+    loadLanguage(selectedLanguage)
+}
+
+function loadContentComponents() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting && entry.target.dataset.component) {
+                loadComponent(entry.target, entry.target.dataset.component)
+                loadLanguage(localStorage.getItem('language') || 'en')
+                observer.unobserve(entry.target)
+            }
+        })
+    }, {threshold: 0.1})
+
+    document.querySelectorAll('[data-component]').forEach(element => {
+        observer.observe(element)
+    })
+}
+
+function loadComponent(parent, component) {
+    fetch(`components/${component}`)
+        .then (res => {
+            return res.text() 
+        })
+        .then(html => {
+            parent.innerHTML = html
+    })
+}
+
+function loadLanguage(lang) {
+    fetch(`language/${lang}.json`)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelectorAll('[data-i18n]').forEach(element => {
+                const key = element?.getAttribute('data-i18n')?.split('.') || []
+                if(key.length > 1) {
+                    const elementText = data[key[0]][key[1]]
+                    element.innerHTML = elementText
+                }
+            })
+    })
+}
